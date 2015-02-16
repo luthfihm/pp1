1a118a89ce91c3fa4126883d72be411ec8b182bf
Luthfi Hamid Masykuri <luthfi_hamid_m@yahoo.co.id>
2015-02-16 09:40:10 +0700

    Edit banner

--- index.php
+++ index.php
@@ -112,7 +112,7 @@ $list_taman = GetAllTaman();
                                         <li><a href="#about">Tentang Kami</a></li>
                                         <li><a href="#services">Alur Kerja</a></li>
                                         <li><a href="#lapor">Lapor!</a></li>
-                                        <li><a href="#clients">Hasil</a></li>
+                                        <li><a href="#clients">Data Keluhan</a></li>
                                         <li><a href="#contact">Kontak</a></li>
                                     </ul>
                                 </div>
@@ -143,6 +143,7 @@ $list_taman = GetAllTaman();
                 <div class="col-md-8 col-md-offset-2 object-non-visible" data-animation-effect="fadeIn">
                     <h1 class="text-center">Selamat <span>Datang</span></h1>
                     <p class="lead text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos debitis provident nulla illum minus enim praesentium repellendus ullam cupiditate reiciendis optio voluptatem, recusandae nobis quis aperiam, sapiente libero ut at.</p>
+                    <div class="smooth-scroll" align="center"><a href="#lapor" class="btn btn-lg btn-primary">Laporkan Keluhan Anda Sekarang!</a></div>
                 </div>
             </div>
         </div>
